0502d4d57d5ce4d0fd4766883d0ac442e8696dfb
SMCampos <sandrocampos72@gmail.com>
2020-10-17 14:56:38 -0300

    atualizando Readme e comentando o código

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # aula_javascript
+
 Introdução JavaScript
+
+Criando uma página HTML5 básica para demonstração de funcionalidades de JavaScript.
+
+Inserido um elemento do tipo button com evento onclick que retorna uma mensagem de agradecimento em forma de alerta
+e com mensagem abaixo do button
+Ao clicar sobre a mensagem abaixo do button será redirecionado para um site externo.
+
+function clicar(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+
+    console.log(document.getElementById("agradecimento"));
+    alert("Obrigado por clicar!!!");
+}
+
+function redirecionar(){
+    window.open("https://uol.com.br/");
+    window.location.href = "https://uol.com.br/";
+} 
+
+Inserindo parágrafos com evento onmouseover e onmouseout
+
+function trocar(elemento){
+    elemento.innerHTML = "Obrigado por passar o mouse!!";
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse!!";
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+    elemento.innerHTML = "Passe o mouse aqui!";
+   //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui!";
+}
+
+Inserindo um select com função onchange
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+
+Colocando uma função que exibe um alert que a página foi carregada
+function load(){
+    alert("Página carregada!");
+}
+
+Criando funções que vão fazer validação e gerar alertas na página.
+
+Função que percorre um array insere e remove elemento, verifica o tamanho do array,
+inverte as posições dos elementos no array e exibe a lista de elementos em um alert.
+
+Função que verifica os dados em um array e mostra em um alert um atributo do objeto.
+
+Função que verifica a idade e retorna uma mensagem em um alert.
+
+Função que percorre os valores através de um for e mostra os resultdos em um alert.
+
+Função que retorna em um alert a data e hora atual.
+
+Função que pega um valor dos parâmetros e substitui por outro valor e retorna em um alert
--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-/*
 function clicar(){
     document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
 
@@ -30,6 +29,7 @@ function funcaoChange(elemento){
     console.log(elemento.value)
 }
 
+//função para validar a idade ao acessar a página
 var validar;
 function validaIdade(idade){
     if (idade >= 18){
@@ -43,22 +43,27 @@ var idade = prompt("Qual sua idade?");
 validaIdade(idade);
 console.log(validar);
 
+
+//Função que pega um valor dos parâmetros e substitui por outro valor e retorna em um alert
 function setReplace(frase, nome, novo_nome){
     return frase.replace(nome, novo_nome)
 }
 alert(setReplace("Vai Japão", "Japão", "Brasil"));
 
+//função que retorna em um alert a soma de dois números
 function soma(n1, n2){
     return n1 + n2;
 }
 alert(soma(5, 2));
 
+//Função que retorna em um alert a data e hora atual
 var d = new Date();
 alert(d);
 alert(d.getMonth());
 alert(d.getDay());
 alert(d.getHours());
 
+//Função que percorre os valores através de um for e mostra os resultdos em um alert
 var count;
 for (count=0; count <=5; count++){
     alert(count);
@@ -71,6 +76,7 @@ while(count < 5){
     count++;
 }
 
+//Função que verifica a idade e retorna uma mensagem em um alert
 var idade = prompt("Qual sua idade?");
 if (idade >= 18){
     alert("Maior de idade!");
@@ -78,14 +84,18 @@ if (idade >= 18){
     alert("Menor de idade!");
 };
 
+//Função que verifica os dados em um array e mostra em um alert um objeto na posição indicada do array
 var frutas = [{nome:"maçã", cor:"vermelha"}, {nome:"pera", cor:"verde"}];
 console.log(frutas);
 alert(frutas[1].nome);
 
-var fruta = {nome:"maçã", cor:"vermelha"};
+//Função que verifica os dados em um array e mostra em um alert um atributo do objeto
+var fruta = [{nome:"maçã", cor:"vermelha"}, {nome:"pera", cor:"verde"}];
 console.log(fruta);
-alert(fruta.cor);
+alert(fruta[0, 1].cor); 
 
+//Função que percorre um array insere e remove elemento, verifica o tamanho do array,
+// inverte as posições dos elementos no array e exibe a lista de elementos em um alert.
 var lista = ["maçã", "pera", "laranja"];
 lista.push("uva");
 lista.pop([3]);
@@ -94,4 +104,4 @@ console.log(lista.toString());
 console.log(lista.join(" | "));
 console.log(lista.length);
 console.log(lista.reverse());
-alert(lista[1]);
+alert(lista);
